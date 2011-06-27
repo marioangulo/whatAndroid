ba6c19783f9902cb1026508663a858ab634c0ec8
Gwindow <tim.mikeladze@gmail.com>
2011-06-27 03:55:44 -0700

    eclipse setup

---
+++ src/what/gui/WhatAndroidActivity.java
@@ -0,0 +1,13 @@
+package what.gui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WhatAndroidActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
