96d01def0eaa78fbf9bb67fa5f7bdd0f5542e2c0
Gwindow <tim.mikeladze@gmail.com>
2011-07-03 22:03:08 -0700

    partial userinforum fix

--- src/api/forum/Threads.java
+++ src/api/forum/Threads.java
@@ -8,119 +8,155 @@ import api.util.Triple;
 
 /**
  * A Thread in the forum
+ * 
  * @author Tim
- *
+ * 
  */
 public class Threads {
 	private String threadTitle;
-	private String threadAuthor;
-	private String threadLastPoster;
+	private UserInForum threadAuthor;
+	private UserInForum threadLastPoster;
 	private String threadUrl;
 	private LinkedList<Post> postList = new LinkedList<Post>();
-	//TODO add userinforum 
-	/*public Threads(String threadTitle,UserInForum userInForum) throws IOException {
-		this.threadTitle = threadTitle;
-		this.threadAuthor = userInForum.getUserName();
-		this.threadLastPoster = threadLastPoster;
-		addPost();
-	}*/
+
 	/**
 	 * Create a new thread
+	 * 
 	 * @param threadTitle
+	 *            title of thread
 	 * @param threadAuthor
+	 *            author
 	 * @param threadLastPoster
-	 * @throws IOException 
+	 *            last poster
+	 * @param threadUrl
+	 *            url of thread
+	 * @throws IOException
 	 */
-	public Threads(String threadTitle, String threadAuthor, String threadLastPoster, String threadUrl) throws IOException {
+	public Threads(String threadTitle, UserInForum threadAuthor, UserInForum threadLastPoster, String threadUrl) throws IOException {
 		this.threadTitle = threadTitle;
 		this.threadAuthor = threadAuthor;
 		this.threadLastPoster = threadLastPoster;
 		this.threadUrl = threadUrl;
 	}
-	/**
-	 * Get title of the thread
-	 * @return thread title
-	 */
+
 	public String getThreadTitle() {
 		return threadTitle;
 	}
+
 	/**
 	 * Get author of the thread
+	 * 
 	 * @return thread author
 	 */
 	public String getThreadAuthor() {
-		return threadAuthor;
+		return threadAuthor.getUserName();
 	}
+
+	/**
+	 * Get user id of author
+	 * 
+	 * @return
+	 */
+	public String getThreadAuthorID() {
+		return threadAuthor.getUserName();
+	}
+
 	/**
 	 * Get last poster in the thread
+	 * 
 	 * @return last poster
 	 */
 	public String getThreadLastPoster() {
-		return threadLastPoster;
+		return threadLastPoster.getUserName();
 	}
+
+	/**
+	 * Get last poster user id
+	 * 
+	 * @return user id of last poster
+	 */
+	public String getThreadLastPosterID() {
+		return threadLastPoster.getUserID();
+	}
+
 	/**
 	 * Get the url of a thread
+	 * 
 	 * @return thread url
 	 */
 	public String getThreadUrl() {
 		return threadUrl;
 	}
+
 	/**
 	 * Add posts to a thread
-	 * @throws IOException 
+	 * 
+	 * @throws IOException
 	 */
 	public void addPosts() throws IOException {
 		for (Triple<String, String, String> p : PostParser.parsePosts(this)) {
-			postList.add(new Post(new UserInForum(p.getA(),p.getB()),p.getC()));
+			postList.add(new Post(new UserInForum(p.getA(), p.getB()), p.getC()));
 		}
 	}
+
 	public String[] getPostUserArray() {
 		String[] s = new String[postList.size()];
-		for(int i=0;i<s.length; i++) {
+		for (int i = 0; i < s.length; i++) {
 			s[i] = postList.get(i).getUserInForum().getUserName();
 		}
 		return s;
 	}
+
 	public String[] getPostUserIDArray() {
 		String[] s = new String[postList.size()];
-		for(int i=0;i<s.length; i++) {
+		for (int i = 0; i < s.length; i++) {
 			s[i] = postList.get(i).getUserInForum().getUserID();
 		}
 		return s;
 	}
+
 	public String[] getPostBodyArray() {
 		String[] s = new String[postList.size()];
-		for(int i=0;i<s.length; i++) {
+		for (int i = 0; i < s.length; i++) {
 			s[i] = postList.get(i).getPostBody();
 		}
 		return s;
 	}
+
 	/**
 	 * Get the original post in a thread
+	 * 
 	 * @return original post
 	 */
 	public Post getOriginalPost() {
 		return postList.getFirst();
 	}
+
 	/**
 	 * Get the last post in a thread
+	 * 
 	 * @return last post
 	 */
 	public Post getLastPost() {
 		return postList.getLast();
 	}
+
 	/**
 	 * Return list of posts
+	 * 
 	 * @return postList
 	 */
 	public LinkedList<Post> getPost() {
 		return postList;
 	}
+
 	/**
 	 * String representation of a thread and everything inside it
 	 */
+	@Override
 	public String toString() {
-		String toReturn = threadTitle + ", " + threadAuthor + ", " + threadLastPoster + "\n" /*+ postList.toString*/;
+		String toReturn = threadTitle + ", " + threadAuthor + ", "
+				+ threadLastPoster + "\n" /* + postList.toString */;
 		return toReturn;
 	}
 }
--- src/api/forum/UserInForum.java
+++ src/api/forum/UserInForum.java
@@ -1,18 +1,23 @@
 package api.forum;
 
 public class UserInForum {
-	String userName;
-	String userID;
+	private String userName;
+	private String userID;
+
 	public UserInForum(String userName, String userID) {
 		this.userName = userName;
 		this.userID = userID;
 	}
+
 	public String getUserName() {
 		return userName;
 	}
+
 	public String getUserID() {
 		return userID;
 	}
+
+	@Override
 	public String toString() {
 		String toReturn = userName + "," + userID;
 		return toReturn;
